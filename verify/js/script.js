1148016de9cff7c809ff13f0c56d3823886fa715
giusene <giusene@users.noreply.github.com>
2022-01-25 16:56:55 +0000

    deploy: b2834b45e413e0b5972b3376d0a4c025850bf798

---
+++ verify/js/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+  switch (true) {
+    case window.location.href.includes('verify'):
+      verify();
+      break;
+    case window.location.href.includes('reset'):
+      reset();
+      break;
+    default:
+      other();
+      break;
+
+  }
+}
+)
+
+
+const verify = () => {
+  const id = window.location.href.split('=')[1];
+  console.log('verify', id);
+  httpUPDATE(`/users/${id}`, { confirmed: true })
+    .then(data => {
+      const container = document.querySelector('.container');
+      const textWrapper = document.querySelector('.waiting');
+      textWrapper.textContent = 'Utente Confermato!';
+      const button = document.createElement('a');
+      button.textContent = "Vai alla pagina di login";
+      button.setAttribute('href', 'https://giusene.github.io/Feisbrut-React-Social/');
+      container.appendChild(button);
+
+      console.log(data)
+    })
+    .catch(err => {
+      const container = document.querySelector('.container');
+      const textWrapper = document.querySelector('.waiting');
+      textWrapper.textContent = "C'è stato un errore!'";
+      const button = document.createElement('a');
+      button.textContent = "Vai alla pagina di login";
+      button.setAttribute('href', 'https://giusene.github.io/Feisbrut-React-Social/');
+      container.appendChild(button);
+      console.log(err)
+    })
+
+}
+
+const reset = () => {
+  const id = window.location.href.split('=')[1];
+  console.log('reset', id);
+}
+
+const other = () => {
+  console.log('other');
+}
+
+
+const httpUPDATE = (resource, data) =>
+  fetch('https://feisbrut.herokuapp.com' + resource, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(data),
+  }).then(response => response.json());
