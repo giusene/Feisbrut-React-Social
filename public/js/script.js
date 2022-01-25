316ac68ecbbcadd7d52858aed42af6e3f1a4340a
g.sen <giu.sene@gmail.com>
2022-01-25 11:18:54 +0100

    feat: other pages

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     switch (true) {
         case window.location.href.includes('verify'):
-            console.log('verify');
+            verify();
             break;
         case window.location.href.includes('reset'):
-            console.log('reset');
+            reset();
             break;
         default:
-            console.log('404');
+            other();
             break;
 
     }
 }
 )
+
+
+const verify = () => {
+  const id = window.location.href.split('=')[1];
+  console.log('verify', id);
+}
+
+const reset = () => {
+  const id = window.location.href.split('=')[1];
+  console.log('reset', id);
+}
+
+const other = () => {
+  console.log('other', id);
+}
